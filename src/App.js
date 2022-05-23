bbf9bac18e338b4a9642c82a0a919cbadb5183b1
ALEXANDER <msumba83@gmail.com>
2022-05-23 22:21:28 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Welcome to the Pokedex</h1>
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
       </header>
     </div>
   );
