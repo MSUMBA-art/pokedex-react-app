6b17ba5190ed046c716afd7f4dc2755b6168e94e
ALEXANDER <msumba83@gmail.com>
2022-05-24 06:56:07 +0200

    use map method to loop through items

--- src/App.js
+++ src/App.js
@@ -4,12 +4,42 @@ import React from "react";
 function App() {
   return (
     <div>
-      <header>
-        <h1>Welcome to the Pokedex</h1>
-        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
-      </header>
+      <Logo />
+      <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
+}
+
+const Logo = () => {
+  const appName = "Alexander's Pokedex";
+
+  return(
+  <header>
+    
+      <h1>{ appName }</h1>
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
+    </header>
+  );
+}
+
+const BestPokemon = () => {
+  const abilities = ['Anticipation', 'Adaptability', 'Run-Away']
+  return (
+    <div>
+      <p>My favorite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map((ability, index) => (
+          <li key={index}>{ability}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const CaughtPokemon = () => {
+  const date = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {date}</p>;
 }
 
 export default App;
